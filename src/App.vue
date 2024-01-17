e882a7d3e427e4f9a163f2b39a868d248b9231f7
Joshua Hamilton-Brown <joshuahb.jhb@gmail.com>
2024-01-17 15:35:04 +0200

    Adding some basic pages for my SPA

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,10 @@
 <template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+    <router-link to="/profile">Portfolio</router-link>
+  </div>
+  <router-view/>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
---
+++ src/views/Portfolio.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="home">
+      Portfolio Info
+    </div>
+  </template>
