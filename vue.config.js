676e173775996829a8ecf73b5b137a03721e36c6
Joshua Hamilton-Brown <joshuahb.jhb@gmail.com>
2024-01-17 15:34:24 +0200

    Removed setup for a MPA, now using SPA with routing

--- vue.config.js
+++ vue.config.js
@@ -2,18 +2,4 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
-  pages: {
-    index: {
-      entry: 'src/pages/Home/main.js',
-      template: 'public/index.html',
-      title: 'Home',
-      chunks: ['chunk-vendors', 'chunk-common', 'index']
-    },
-    about: {
-      entry: 'src/pages/Index/main.js',
-      template: 'public/index.html',
-      title: 'About Me',
-      chunks: ['chunk-vendors', 'chunk-common', 'index']
-    }
-  }
 })
